7d7b889ff3f71cd868a9738d68a4a135812a3881
MNKhamees <m.khamess2001@yahoo.com>
2026-03-10 14:43:29 +0300

    worked on creating the data

--- analysis.sql
+++ analysis.sql
@@ -11,3 +11,32 @@ create table students (
     education_level char(1),
     track varchar(255),
     student_GPA float);
+
+    create table teachers (
+    id int primary key,
+    teacher_name varchar(255),
+    birth_date date,
+    sex char(1),
+    teacher_email varchar(255),
+    office_number char(1)
+    );
+    
+    create table courses (
+    id int primary key,
+    course_name varchar(255));
+
+    SHOW TABLES;
+
+    SELECT * FROM students;
+
+    insert into students values 
+    (1, 'Faisal', '2007-03-12', 'M', '2024-08-20', 'f.harbi@mail.com', 'H', 'Science', 4.92),
+    (2, 'Mohammed', '2001-03-12', 'm', '2020-09-01', 'Mohammed@mail.com', 
+
+    DELETE FROM students WHERE id = 0;
+
+    DELETE FROM students WHERE id BETWEEN 2 AND 30;
+
+    alter table students MODIFY education_level char(1);
+
+    UPDATE students set track = 'Art' where id = (1+7_3);
